d26b10df2601b462741698c31402308d4452910b
kali <kali@kali.Kali>
2022-03-16 13:08:50 +0400

    ALE VIO VIO ALE

---
+++ src/Main.kt
@@ -0,0 +1,28 @@
+//////////////////////////////// TASK ALE 2 ///////////////////////////////////////
+//fun main() {
+//    val word = "ale ela"
+//
+//    var aleWord = ""
+//    var didiSigrdze = word.length
+//
+//    for (i in (didiSigrdze - 1) downTo 0) {
+//        aleWord = aleWord + word[i]
+//    }
+//
+//    if (word.equals(aleWord, ignoreCase = true)) {
+//        println("aleuria")
+//    } else {
+//        println("arari aleuri")
+//    }
+//}
+
+
+
+fun main() {
+
+    val arrayList = [1, 2, 3, 4, 5, 6]
+    val even = 0
+
+    
+
+}
